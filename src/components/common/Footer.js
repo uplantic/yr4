0271ec9f9b218ac3620ad03357573d391f54a345
uplantic <dev@uplantic.com>
2025-02-17 16:24:52 +0100

    Changed bg image Footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const navigation = {
   main: [
@@ -13,7 +14,7 @@ const navigation = {
 const Footer = () => {
   return (
     <footer className="relative bg-stone-900 md:h-[800px]">
-      <div className="absolute inset-0 bg-cover bg-bottom bg-[url(../images/footer.jpg)]" />
+      <StaticImage src="../../images/footer.jpg" alt="" layout="fullWidth" placeholder="blurred" className="object-cover object-bottom" />
       <div className="relative mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <nav aria-label="Footer" className="-mb-6 flex flex-wrap justify-center gap-x-12 gap-y-3 text-sm/6">
           {navigation.main.map((item) => (
@@ -22,7 +23,7 @@ const Footer = () => {
             </Link>
           ))}
         </nav>
-        <h1 className="mt-10 text-center text-lg/6 text-gun-metal">&copy; {new Date().getFullYear()} &middot; Asteroid 2024 YR4</h1>
+        <h1 className="mt-10 text-center text-lg/6 text-gun-metal-1">&copy; {new Date().getFullYear()} &middot; Asteroid 2024 YR4</h1>
       </div>
     </footer>
   );
